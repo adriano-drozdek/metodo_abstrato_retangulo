e7d5bf35ed178f4b11562e9bbf1ce051ba5072aa
Adriano Drozdek <adrianodrozdek2791@gmail.com>
2022-04-20 14:20:06 -0300

    Identificadores de figura (circulo/retângulo)

--- src/programa/Projeto.java
+++ src/programa/Projeto.java
@@ -1,8 +1,13 @@
 package programa;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
+import entities.Circulo;
+import entities.Formato;
+import entities.Retangulo;
 import entities.enums.Cores;
 
 public class Projeto {
@@ -12,6 +17,8 @@ public class Projeto {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
+		List<Formato> list = new ArrayList<>();
+		
 		System.out.println("Informe a quantidade de figuras: ");
 		int n = sc.nextInt();
 
@@ -21,7 +28,20 @@ public class Projeto {
 			char ch = sc.next().charAt(0);
 			System.out.print("Cor (PRETO/AZUL/VERMELHO) ");
 			Cores cor = Cores.valueOf(sc.next());
-
+			
+			if (ch == 'r') {
+				System.out.println("Largura: ");
+				double largura = sc.nextDouble();
+				System.out.println("Altura: ");
+				double altura = sc.nextDouble();
+				list.add(new Retangulo(cor, largura, altura));
+			}
+			else {
+				System.out.println("Raio: ");
+				double raio = sc.nextDouble();
+				list.add(new Circulo(cor, raio));
+			}
+			
 		}
 
 		sc.close();
