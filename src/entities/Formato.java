39aedb69a88917005c626836980103fc788eb40e
Adriano Drozdek <adrianodrozdek2791@gmail.com>
2022-04-20 10:56:12 -0300

    Implementando COR.

--- src/entities/Formato.java
+++ src/entities/Formato.java
@@ -1,5 +1,25 @@
 package entities;
 
+import entities.enums.Cores;
+
 public class Formato {
+
+	private Cores cor;
+
+	public Formato() {
+
+	}
+
+	public Formato(Cores cor) {
+		this.cor = cor;
+	}
+
+	public Cores getCor() {
+		return cor;
+	}
+
+	public void setCor(Cores cor) {
+		this.cor = cor;
+	}
 
 }
