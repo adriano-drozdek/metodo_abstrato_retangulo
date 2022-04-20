1e7f976e0acd7ee0ae1f66a5e6814947c838088e
Adriano Drozdek <adrianodrozdek2791@gmail.com>
2022-04-20 13:33:29 -0300

    Criado projeto principal, projeto iniciado

---
+++ src/programa/Projeto.java
@@ -0,0 +1,28 @@
+package programa;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.enums.Cores;
+
+public class Projeto {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Informe a quantidade de figuras: ");
+		int n = sc.nextInt();
+
+		for (int i = 1; i <= n; i++) {
+			System.out.print("Figura número #" + i);
+			System.out.print("Retângulo ou circulo (r/c) ");
+			char ch = sc.next().charAt(0);
+
+		}
+
+		sc.close();
+	}
+
+}
